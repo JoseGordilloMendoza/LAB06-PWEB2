8eb6a6f2fd3525bc9b1e6d3f7cc9c72d409ebad4
Jose Gordillo <you@example.comjgordillome@unsa.edu.oe>
2023-06-28 18:01:48 -0500

    agregando urls a las urls generales

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -6,4 +6,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('',include('destinos.urls')),
     path('admin/', admin.site.urls),
-]
+    path('cuenta/',include('cuenta.urls'))
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)   
