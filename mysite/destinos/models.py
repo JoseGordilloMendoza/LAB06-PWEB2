3a0dccc5b6af1303d4243e08bcd5690560e4a3d2
Jose Gordillo <you@example.comjgordillome@unsa.edu.oe>
2023-06-28 09:56:02 -0500

    Entorno virtual y proyecto

---
+++ mysite/destinos/models.py
@@ -0,0 +1,13 @@
+from django.conf import settings
+from PIL import Image
+from django.db import models
+
+class DestinosTuristicos(models.Model):
+    nombreCiudad = models.CharField(max_length=100)
+    descripcionCiudad = models.TextField()
+    imagenCiudad = models.ImageField(upload_to='imagenes/')
+    precioTour = models.DecimalField(max_digits=8, decimal_places=2)
+    ofertaTour = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.nombreCiudad
